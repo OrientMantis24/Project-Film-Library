5065e554348b5d88162e44cdb32896db2abc5be4
Bartosz Ociepka <bartosz.ociepka96@gmail.com>
2016-02-04 18:21:25 +0100

    Added database project .sqlproj file

--- FilmLibrary/FilmLibrary/FilmDatabase.cs
+++ FilmLibrary/FilmLibrary/FilmDatabase.cs
@@ -11,14 +11,14 @@ namespace FilmLibrary
     {
         public static SqlConnection GetConnection()
         {
-            string connStr = @"Data Source=(LocalDB)\v12.0;AttachDbFilename=|DataDirectory|database1.mdf;Integrated Security=True";
+            string connStr = @"Data Source=BARTOSZOCIEPKA\SQLEXPRESS;Initial Catalog=Films3;Integrated Security=True;";
             SqlConnection conn = new SqlConnection(connStr);
             return conn;
         }
 
         public static void AddFilm(string filmTitle, string filmDescription, string filmDirector, string filmsGenres)
         {
-            string insStmt = "INSERT INTO Films(Title, Description, Director, Genres) Values (@filmTitle, @filmDescription, @filmDirector, @FilmGenres)";
+            string insStmt = "INSERT INTO Films3(Title, Description, Director, Genres) Values (@filmTitle, @filmDescription, @filmDirector, @FilmGenres)";
             SqlConnection conn = GetConnection();
             SqlCommand insCmd = new SqlCommand(insStmt, conn);
             insCmd.Parameters.AddWithValue("@filmTitle", filmTitle);
@@ -34,7 +34,7 @@ namespace FilmLibrary
         {
             List<Films> filmsList = new List<Films>();
             SqlConnection conn = GetConnection();
-            string selStmt = "SELECT * FROM FILMS ORDER BY TITLE";
+            string selStmt = "SELECT * FROM Films3 ORDER BY TITLE";
             SqlCommand selCmd = new SqlCommand(selStmt, conn);
             try
             {
@@ -46,6 +46,7 @@ namespace FilmLibrary
                     film.FilmId = (int)reader["Id"];
                     film.FilmTitle = reader["Title"].ToString();
                     film.FilmDescription = reader["Description"].ToString();
+                    film.FilmDirector = reader["Director"].ToString();
                     film.FilmGenres = reader["Genres"].ToString();
                     filmsList.Add(film);
                 }
@@ -55,6 +56,16 @@ namespace FilmLibrary
             catch (SqlException ex) { throw ex; }
             finally { conn.Close(); }
             return filmsList;
+        }
+
+        public static void EditFilm(int filmId, string filmTitle, string filmDescription, string filmDirector, string filmsGenres)
+        {
+            string insStmt = "UPDATE Films SET Title='" + filmTitle + "', Description='" + filmDescription + "', Director='" + filmDirector + "', Genres='" + filmsGenres + "' WHERE Id='" + filmId + "'";
+            SqlConnection conn = GetConnection();
+            SqlCommand insCmd = new SqlCommand(insStmt, conn);
+            try { conn.Open(); insCmd.ExecuteNonQuery(); }
+            catch (SqlException ex) { throw ex; }
+            finally { conn.Close(); }
         }
     }
 }
