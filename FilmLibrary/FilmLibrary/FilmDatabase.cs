77650d1f58d29251eb77d43e021b1676d7225130
Bartosz Ociepka <bartosz.ociepka96@gmail.com>
2016-02-08 23:59:28 +0100

    Viewing films

--- FilmLibrary/FilmLibrary/FilmDatabase.cs
+++ FilmLibrary/FilmLibrary/FilmDatabase.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Core.Objects;
 
 namespace FilmLibrary
 {
@@ -17,7 +19,7 @@ namespace FilmLibrary
             return conn;
         }
 
-        public static void AddFilm(string filmTitle, string filmDescription, string filmDirector, string filmsGenres)
+        public static void AddFilm(string filmTitle, string filmDescription, string filmDirector, string filmGenres)
         {
             //string insStmt = "INSERT INTO FILMS(Title, Description, Director, Genres) Values (@filmTitle, @filmDescription, @filmDirector, @FilmGenres)";
             //SqlConnection conn = GetConnection();
@@ -32,7 +34,7 @@ namespace FilmLibrary
 
             using (var ctx = new FilmsContextClass())
             {
-                FILMS film = new FILMS() { Title = "Bioshock" };
+                FILMS film = new FILMS() { Title = filmTitle, Description = filmDescription, Director = filmDirector, Genres = filmGenres };
 
                 ctx.Films.Add(film);
                 ctx.SaveChanges();
@@ -42,28 +44,46 @@ namespace FilmLibrary
         public static List<FILMS> GetFilm()
         {
             List<FILMS> filmsList = new List<FILMS>();
-            SqlConnection conn = GetConnection();
-            string selStmt = "SELECT * FROM FILMS ORDER BY TITLE";
-            SqlCommand selCmd = new SqlCommand(selStmt, conn);
-            try
+
+            //SqlConnection conn = GetConnection();
+            //string selStmt = "SELECT * FROM FILMS ORDER BY TITLE";
+            //SqlCommand selCmd = new SqlCommand(selStmt, conn);
+            //try
+            //{
+            //    conn.Open();
+            //    SqlDataReader reader = selCmd.ExecuteReader();
+            //    while(reader.Read())
+            //    {
+            //        FILMS film = new FILMS();
+            //        film.Id = (int)reader["Id"];
+            //        film.Title = reader["Title"].ToString();
+            //        film.Description = reader["Description"].ToString();
+            //        film.Director = reader["Director"].ToString();
+            //        film.Genres = reader["Genres"].ToString();
+            //        filmsList.Add(film);
+            //    }
+            //    reader.Close();
+
+            //}
+            //catch (SqlException ex) { throw ex; }
+            //finally { conn.Close(); }
+            //return filmsList;
+
+            
+
+            using (var context = new FilmsContextClass())
             {
-                conn.Open();
-                SqlDataReader reader = selCmd.ExecuteReader();
-                while(reader.Read())
-                {
-                    FILMS film = new FILMS();
-                    film.Id = (int)reader["Id"];
-                    film.Title = reader["Title"].ToString();
-                    film.Description = reader["Description"].ToString();
-                    film.Director = reader["Director"].ToString();
-                    film.Genres = reader["Genres"].ToString();
-                    filmsList.Add(film);
-                }
-                reader.Close();
+
+                
+                    foreach (var film in context.Films)
+                    {
+                        filmsList.Add(film);
+                    }
+                  
+                
+
 
             }
-            catch (SqlException ex) { throw ex; }
-            finally { conn.Close(); }
             return filmsList;
         }
 
